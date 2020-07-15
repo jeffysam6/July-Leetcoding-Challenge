af1aa89e2a3a5fcf9eda2df3a91e9b26802a936a
jeffysam6 <jeffysam02@gmail.com>
2020-07-15 20:15:45 +0530

    two pointer approach

---
+++ day-15-reverse-words-in-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        
+        s = s.split()
+        
+        l = 0
+        r = len(s)-1
+        
+        while(l < r):
+            
+            s[l],s[r] = s[r],s[l]
+            
+            l += 1
+            r -= 1
+        
+        
+        return ' '.join(s)
