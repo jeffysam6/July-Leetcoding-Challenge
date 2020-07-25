df9ca8e9587a348ed54dfeeaa2229e0140a8c221
jeffysam6 <jeffysam02@gmail.com>
2020-07-25 10:35:21 +0530

    recursive dfs

--- day-24-all-path-from-source-to-target.py
+++ day-24-all-path-from-source-to-target.py
@@ -18,3 +18,26 @@ class Solution:
                     stack.append(p + [i])
         
         return ans
+
+
+
+
+# class Solution:
+#     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+#         ans = []
+        
+#         def dfs(curr,path):
+            
+#             if(curr == len(graph) - 1):
+#                 ans.append(path + [curr])
+            
+            
+#             else:
+#                 for i in graph[curr]:
+#                     dfs(i,path + [curr])
+                    
+        
+#         dfs(0,[])
+        
+#         return ans
