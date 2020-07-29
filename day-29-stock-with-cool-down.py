15d9d638ea22251d26dfc30825d38309a6a17d8c
jeffysam6 <jeffysam02@gmail.com>
2020-07-30 00:33:51 +0530

    upsolved using dp

--- day-29-stock-with-cool-down.py
+++ day-29-stock-with-cool-down.py
@@ -1,27 +1,35 @@
 class Solution:
     def maxProfit(self, arr: List[int]) -> int:
         
-        mini = float('inf')
-
-        maxi = 0
 
 
-        max_prof = 0
+        dp = [[None,None] for _ in range(len(arr))]
 
-        i = 0
+        if(len(arr) <=1 ):
+            return(0)
 
-        while(i < len(arr)):
+        if(len(arr) == 2 and arr[1] > arr[0]):
+            return(arr[1] - arr[0])
 
-            mini = min(mini,arr[i])
+        elif(len(arr) == 2 and arr[1] < arr[0]):
+            return(0)
 
-            # print(mini)
 
-            if(arr[i] > mini):
-                max_prof += (arr[i] -mini)
-                mini = arr[i]
 
-                i += 1
+        dp[0][0] = 0
 
-            i += 1
-            
-        return max_prof
+        dp[0][1] = -arr[0]
+
+        dp[1][0] = max(dp[0][0],dp[0][1] + arr[1])
+
+        dp[1][1] = max(dp[0][1],dp[0][0] - arr[1])
+
+        for i in range(2,len(arr)):
+
+            dp[i][0] = max(dp[i-1][0],dp[i-1][1] + arr[i])
+
+            dp[i][1] = max(dp[i-1][1],dp[i-2][0] - arr[i])
+
+
+
+        return dp[-1][0]
